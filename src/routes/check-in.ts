24b81993f1934922f1b9d41c1124fded20d7ce59
Danrlei <danrleidalfre@gmail.com>
2024-04-03 19:46:47 -0300

    add check in

---
+++ src/routes/check-in.ts
@@ -0,0 +1,36 @@
+import {FastifyInstance} from "fastify";
+import {ZodTypeProvider} from "fastify-type-provider-zod";
+import {z} from "zod";
+import {prisma} from "../lib/prisma";
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>()
+    .get('/attendee/:attendeeId/check-in', {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int()
+        }),
+        response: {
+          201: z.null()
+        }
+      }
+    }, async (request, reply) => {
+      const {attendeeId} = request.params
+
+      const attendeeCheckIn = await prisma.checkIn.findUnique({
+        where: {
+          attendeeId
+        }
+      })
+
+      if (attendeeCheckIn) {
+        throw new Error('Attendee already check in')
+      }
+
+      await prisma.checkIn.create({
+        data: {attendeeId}
+      })
+
+      return reply.status(201).send()
+    })
+}
