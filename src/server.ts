c2bce44e48c701b8456f4180de93f2e2b1ce3603
Danrlei <danrleidalfre@gmail.com>
2024-04-04 18:26:18 -0300

    add docs with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 import fastify from "fastify";
-import {serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
+import {jsonSchemaTransform, serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
 import {createEvent} from "./routes/create-event";
 import {registerForEvent} from "./routes/register-for-event";
 import {getEvent} from "./routes/get-event";
 import {getAttendeeBadge} from "./routes/get-attendee-badge";
 import {checkIn} from "./routes/check-in";
 import {getAttendeesEvent} from "./routes/get-attendees-event";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description: 'Docs PassIn',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
 
 app.register(createEvent)
 app.register(registerForEvent)
